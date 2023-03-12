6d056703164ed406adeb852809646344a68c8a97
Mitali Potnis <113000033+mitali-p42@users.noreply.github.com>
2023-03-12 18:26:47 -0400

    Added validation dataset for evaluation

--- src/train_retrieval.py
+++ src/train_retrieval.py
@@ -10,6 +10,8 @@ from datasets import load_dataset
 
 fr_train_dataset = load_dataset('json', data_files='./data/splits/FrDoc2BotRetrieval_train.json')['train']
 vi_train_dataset = load_dataset('json', data_files='./data/splits/ViDoc2BotRetrieval_train.json')['train']
+vi_train_dataset = load_dataset('json', data_files="data/splits/ViDoc2BotRetrieval_train.json")["train"]
+vi_val_dataset = load_dataset('json', data_files="data/splits/ViDoc2BotRetrieval_val.json")["train"]
 
 # fr_train_dataset_origin = MsDataset.load(
 #     'DAMO_ConvAI/FrDoc2BotRetrieval',
@@ -22,6 +24,7 @@ vi_train_dataset = load_dataset('json', data_files='./data/splits/ViDoc2BotRetri
 # breakpoint()
 
 train_dataset = [x for dataset in [fr_train_dataset, vi_train_dataset] for x in dataset]
+val_dataset = [y for dataset in [fr_val_dataset, vi_val_dataset] for y in dataset]
 
 all_passages = []
 for file_name in ['fr', 'vi']:
@@ -32,7 +35,7 @@ cache_path = snapshot_download('DAMO_ConvAI/nlp_convai_retrieval_pretrain', cach
 trainer = DocumentGroundedDialogRetrievalTrainer(
     model=cache_path,
     train_dataset=train_dataset,
-    eval_dataset=train_dataset,
+    eval_dataset=val_dataset,
     all_passages=all_passages)
 
 trainer.train(
